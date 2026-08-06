a057542fbc38538a69870f961efc033facdbea39
HaojackYT <tangchihao4954@gmail.com>
2026-08-06 19:10:45 +0700

    refactor(patient-service): remove @Table annotation from Patient entity

--- patient-service/src/main/java/com/pm/patient_service/model/Patient.java
+++ patient-service/src/main/java/com/pm/patient_service/model/Patient.java
@@ -4,14 +4,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.UUID;
 
 @Entity
-@Table(name = "patients")
 public class Patient {
 
     @Id
