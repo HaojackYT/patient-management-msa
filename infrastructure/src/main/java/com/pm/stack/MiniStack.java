9a944fd8ed1c7750acf7286963ed4bd4b01efb71
HaojackYT <tangchihao4954@gmail.com>
2026-08-08 18:58:33 +0700

    feat(infrastructure): implement ECS Fargate and MSK cluster for microservices
    - Create ECS Fargate task definitions and services for containerized backends.
    - Add Amazon MSK cluster to enable managed Kafka event streaming.
    - Configure CloudWatch log groups to ensure centralized container observability.
    - Establish a scalable, serverless deployment foundation on AWS.

--- infrastructure/src/main/java/com/pm/stack/MiniStack.java
+++ infrastructure/src/main/java/com/pm/stack/MiniStack.java
@@ -1,5 +1,10 @@
 package com.pm.stack;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import software.amazon.awscdk.App;
 import software.amazon.awscdk.AppProps;
 import software.amazon.awscdk.BootstraplessSynthesizer;
@@ -7,10 +12,24 @@ import software.amazon.awscdk.RemovalPolicy;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.Token;
+import software.amazon.awscdk.services.ec2.ISubnet;
 import software.amazon.awscdk.services.ec2.InstanceClass;
 import software.amazon.awscdk.services.ec2.InstanceSize;
 import software.amazon.awscdk.services.ec2.InstanceType;
 import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.ecs.AwsLogDriverProps;
+import software.amazon.awscdk.services.ecs.CloudMapNamespaceOptions;
+import software.amazon.awscdk.services.ecs.Cluster;
+import software.amazon.awscdk.services.ecs.ContainerDefinitionOptions;
+import software.amazon.awscdk.services.ecs.ContainerImage;
+import software.amazon.awscdk.services.ecs.FargateService;
+import software.amazon.awscdk.services.ecs.FargateTaskDefinition;
+import software.amazon.awscdk.services.ecs.LogDriver;
+import software.amazon.awscdk.services.ecs.PortMapping;
+import software.amazon.awscdk.services.ecs.Protocol;
+import software.amazon.awscdk.services.logs.LogGroup;
+import software.amazon.awscdk.services.logs.RetentionDays;
+import software.amazon.awscdk.services.msk.CfnCluster;
 import software.amazon.awscdk.services.rds.Credentials;
 import software.amazon.awscdk.services.rds.DatabaseInstance;
 import software.amazon.awscdk.services.rds.DatabaseInstanceEngine;
@@ -20,86 +39,183 @@ import software.amazon.awscdk.services.route53.CfnHealthCheck;
 
 public class MiniStack extends Stack {
 
-    private final Vpc vpc;
+	private final Vpc vpc;
+	private final Cluster ecsCluster;
 
-    private Vpc createVpc() {
+	private Vpc createVpc() {
 
-        return Vpc.Builder.create(this, "PatientManagementVpc")
-                .vpcName("PatientManagementVpc")
-                .maxAzs(2) // Avalilability Zones: 2
-                .build();
-    }
+		return Vpc.Builder.create(this, "PatientManagementVpc")
+				.vpcName("PatientManagementVpc")
+				// Avalilability Zones (AZs)
+				.maxAzs(2)
+				.build();
+	}
 
-    private DatabaseInstance createDatabase(String id, String dbName) {
+	// auth-service.patient-management.local
+	private Cluster createEcsCluster() {
 
-        return DatabaseInstance.Builder
-                .create(this, id)
-                .engine(DatabaseInstanceEngine.postgres(
-                        PostgresInstanceEngineProps.builder()
-                                .version(PostgresEngineVersion.VER_18_3)
-                                .build()))
-                .vpc(vpc)
-                // instanceType: a combination of CPU, memory, storage, and networking capacity
-                .instanceType(InstanceType.of(InstanceClass.BURSTABLE2, InstanceSize.MICRO))
-                .allocatedStorage(20) // 20 GB
-                .credentials(Credentials.fromGeneratedSecret("admin_user"))
-                .databaseName(dbName)
-                // destroy the database when the stack is deleted
-                .removalPolicy(RemovalPolicy.DESTROY)
-                .build();
-    }
+		return Cluster.Builder.create(this, "PatientManagementCluster")
+				.vpc(vpc)
+				// Cloud Map namespace: service discovery in AWS ECS
+				// allow microservices to find and communicate with each other
+				.defaultCloudMapNamespace(CloudMapNamespaceOptions.builder()
+						.name("patient-management.local")
+						.build())
+				.build();
+	}
 
-    private CfnHealthCheck createHealthCheck(DatabaseInstance db, String id) {
+	private DatabaseInstance createDatabase(String id, String dbName) {
 
-        return CfnHealthCheck.Builder.create(this, id)
-                .healthCheckConfig(CfnHealthCheck.HealthCheckConfigProperty.builder()
-                        .type("TCP")
-                        .port(Token.asNumber(db.getDbInstanceEndpointPort()))
-                        .ipAddress(db.getDbInstanceEndpointAddress())
-                        .requestInterval(30) // seconds
-                        // try 3 times once every 30 seconds before report a failure
-                        .failureThreshold(3)
-                        .build())
-                .build();
-    }
+		return DatabaseInstance.Builder
+				.create(this, id)
+				.engine(DatabaseInstanceEngine.postgres(
+						PostgresInstanceEngineProps.builder()
+								.version(PostgresEngineVersion.VER_18_3)
+								.build()))
+				.vpc(vpc)
+				// instanceType: a combination of CPU, memory, storage, and networking capacity
+				.instanceType(InstanceType.of(InstanceClass.BURSTABLE2, InstanceSize.MICRO))
+				.allocatedStorage(20) // 20 GB
+				.credentials(Credentials.fromGeneratedSecret("admin_user"))
+				.databaseName(dbName)
+				// destroy the database when the stack is deleted
+				.removalPolicy(RemovalPolicy.DESTROY)
+				.build();
+	}
 
-    public MiniStack(final App scope, final String id, final StackProps props) {
+	private CfnHealthCheck createHealthCheck(DatabaseInstance db, String id) {
 
-        super(scope, id, props);
+		return CfnHealthCheck.Builder.create(this, id)
+				.healthCheckConfig(CfnHealthCheck.HealthCheckConfigProperty.builder()
+						.type("TCP")
+						.port(Token.asNumber(db.getDbInstanceEndpointPort()))
+						.ipAddress(db.getDbInstanceEndpointAddress())
+						.requestInterval(30) // seconds
+						// try 3 times once every 30 seconds before report a failure
+						.failureThreshold(3)
+						.build())
+				.build();
+	}
 
-        this.vpc = createVpc();
+	private CfnCluster createMskCluster() {
 
-        // RDS
-        DatabaseInstance authServiceDb = createDatabase(
-                "AuthServiceDb", "auth-service-db");
-        DatabaseInstance patientServiceDb = createDatabase(
-                "PatientServiceDb", "patient-service-db");
+		return CfnCluster.Builder.create(this, "MskCluster")
+				.clusterName("kafka-cluster")
+				.kafkaVersion("2.8.1")
+				.numberOfBrokerNodes(1)
+				// Connect VPC to all the broker nodes in the cluster using private subnets
+				.brokerNodeGroupInfo(CfnCluster.BrokerNodeGroupInfoProperty.builder()
+						.instanceType("kafka.t3.small")
+						.clientSubnets(vpc.getPrivateSubnets().stream()
+								.map(ISubnet::getSubnetId)
+								.collect(Collectors.toList()))
+						// Specify how many brokers get distributed across AZs
+						.brokerAzDistribution("DEFAULT")
+						.build())
+				.build();
+	}
 
-        CfnHealthCheck authServiceDbHealthCheck = createHealthCheck(
-                authServiceDb, "AuthServiceDbHealthCheck");
-        CfnHealthCheck patientServiceDbHealthCheck = createHealthCheck(
-                patientServiceDb, "PatientServiceDbHealthCheck");
-    }
+	// FargateService: a type of ECS service
+	// easy to start, stop and scale ECS task that run in difference containers
+	private FargateService createFargateService(String id, String imageName, List<Integer> ports,
+			DatabaseInstance db, Map<String, String> additionalEnvVars) {
 
-    public static void main(final String[] args) {
+		FargateTaskDefinition taskDefinition = FargateTaskDefinition.Builder.create(this, id + "Task")
+				.cpu(256) // cpu units
+				.memoryLimitMiB(512) // MB
+				.build();
 
-        // AWS CDK application
-        // outdir: is where the cloudformation template will be generated
-        App app = new App(AppProps.builder().outdir("cdk.out").build());
+		ContainerDefinitionOptions.Builder containerOptions = ContainerDefinitionOptions.builder()
+				.image(ContainerImage.fromRegistry(imageName))
+				.portMappings(ports.stream()
+						.map(port -> PortMapping.builder()
+								.containerPort(port)
+								.hostPort(port)
+								.protocol(Protocol.TCP)
+								.build())
+						.toList())
+				.logging(LogDriver.awsLogs(AwsLogDriverProps.builder()
+						.logGroup(LogGroup.Builder.create(this, id + "LogGroup")
+								.logGroupName("/ecs/" + imageName)
+								.removalPolicy(RemovalPolicy.DESTROY)
+								.retention(RetentionDays.ONE_DAY)
+								.build())
+						.build()));
 
-        // Additional properties that can be applied to the stack
-        // synthesizer: convert code to cloudformation template
-        // BootstraplessSynthesizer: skip inital boostrapping of the cdk environment
-        StackProps stackProps = StackProps.builder()
-                .synthesizer(new BootstraplessSynthesizer())
-                .build();
+		Map<String, String> envVars = new HashMap<>(additionalEnvVars);
+		envVars.put("SPRING_KAFKA_BOOTSTRAP_SERVERS", "localhost:9092");
 
-        new MiniStack(app, "MiniStack", stackProps);
+		if (additionalEnvVars != null) {
+			envVars.putAll(additionalEnvVars);
+		}
 
-        // stackProps + miniStack = cloudformation template
-        app.synth();
+		if (db != null) {
+			envVars.put("SPRING_DATASOURCE_URL", "jdbc:postgresql://%s:%s/%s-db".formatted(
+					db.getDbInstanceEndpointAddress(),
+					db.getDbInstanceEndpointPort(),
+					imageName));
 
-        System.out.println("CloudFormation template generated in cdk.out directory.");
-    }
+			envVars.put("SPRING_DATASOURCE_USERNAME", "admin_user");
+			envVars.put("SPRING_DATASOURCE_PASSWORD",
+					db.getSecret().secretValueFromJson("password").toString());
+			envVars.put("SPRING_JPA_HIBERNATE_DDL_AUTO", "update");
+			envVars.put("SPRING_SQL_INIT_MODE", "always");
+			// Make sure the database is ready
+			envVars.put("SPRING_DATASOURCE_HIKARI_INITIALIZATION_FAIL_TIMEOUT", "600000");
+		}
+
+		containerOptions.environment(envVars);
+		// image <-> container <-> taskDefinition
+		taskDefinition.addContainer(imageName + "Container", containerOptions.build());
+
+		return FargateService.Builder.create(this, id)
+				.cluster(ecsCluster)
+				.taskDefinition(taskDefinition)
+				.assignPublicIp(false)
+				.serviceName(imageName)
+				.build();
+	}
+
+	public MiniStack(final App scope, final String id, final StackProps props) {
+
+		super(scope, id, props);
+		this.vpc = createVpc();
+
+		// RDS
+		DatabaseInstance authServiceDb = createDatabase(
+				"AuthServiceDb", "auth-service-db");
+		DatabaseInstance patientServiceDb = createDatabase(
+				"PatientServiceDb", "patient-service-db");
+		CfnHealthCheck authServiceDbHealthCheck = createHealthCheck(
+				authServiceDb, "AuthServiceDbHealthCheck");
+		CfnHealthCheck patientServiceDbHealthCheck = createHealthCheck(
+				patientServiceDb, "PatientServiceDbHealthCheck");
+
+		// MSK
+		CfnCluster mskCluster = createMskCluster();
+
+		this.ecsCluster = createEcsCluster();
+	}
+
+	public static void main(final String[] args) {
+
+		// AWS CDK application
+		// outdir: is where the cloudformation template will be generated
+		App app = new App(AppProps.builder().outdir("cdk.out").build());
+
+		// Additional properties that can be applied to the stack
+		// synthesizer: convert code to cloudformation template
+		// BootstraplessSynthesizer: skip inital boostrapping of the cdk environment
+		StackProps stackProps = StackProps.builder()
+				.synthesizer(new BootstraplessSynthesizer())
+				.build();
+
+		new MiniStack(app, "MiniStack", stackProps);
+
+		// stackProps + miniStack = cloudformation template
+		app.synth();
+
+		System.out.println("CloudFormation template generated in cdk.out directory.");
+	}
 
 }
