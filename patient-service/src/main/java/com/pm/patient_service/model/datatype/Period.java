cb02d65ec0da15dae287110cbf9fc321d86995ea
HaojackYT <tangchihao4954@gmail.com>
2026-08-18 19:38:37 +0700

    refactor(patient-service): replace ScriptAssert with custom ValidatePeriod annotation and add Embeddable

--- patient-service/src/main/java/com/pm/patient_service/model/datatype/Period.java
+++ patient-service/src/main/java/com/pm/patient_service/model/datatype/Period.java
@@ -1,10 +1,12 @@
 package com.pm.patient_service.model.datatype;
 
-import org.hibernate.validator.constraints.ScriptAssert;
+import com.pm.patient_service.annotation.ValidatePeriod;
 
+import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.Pattern;
 
-@ScriptAssert(lang = "javascript", script = "_this.start <= _this.end", message = "Start date time <= end date")
+@Embeddable
+@ValidatePeriod
 public class Period {
 
     // YYYY: 0001 -> 9999 ([0-9]([0-9]([0-9][1-9]|[1-9]0)|[1-9]00)|[1-9]000)
@@ -13,7 +15,7 @@ public class Period {
     // HH: 00 -> 23 ([01][0-9]|2[0-3])
     // MM: 00 -> 59 [0-5][0-9]
     // SS: 00 -> 59 ([0-5][0-9]|60)
-    // MS: .0 -> .9 (\\.[0-9]+)? (Optional)
+    // MS: 1 to infinite decimal places (\\.[0-9]+)? (Optional)
     // TZ: 00:00 -> 14:00 Z or +HH:MM or -HH:MM
     // (Z|(\\+|-)((0[0-9]|1[0-3]):[0-5][0-9]|14:00))
     private static final String DATE_TIME_REGEX = "([0-9]([0-9]([0-9][1-9]|[1-9]0)|[1-9]00)|[1-9]000)(-(0[1-9]|1[0-2])(-(0[1-9]|[1-2][0-9]|3[0-1])(T([01][0-9]|2[0-3]):[0-5][0-9]:([0-5][0-9]|60)(\\.[0-9]+)?(Z|(\\+|-)((0[0-9]|1[0-3]):[0-5][0-9]|14:00)))?)?)?";
